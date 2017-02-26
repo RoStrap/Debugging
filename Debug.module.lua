1097a64dbb4d7dbe9551dc37c2e0d2b9ac85e6cc
Validark <NarrevRBX@gmail.com>
2017-02-26 04:09:22 -0600

    Update Debug.module.lua

--- Debug.module.lua
+++ Debug.module.lua
@@ -13,7 +13,7 @@ end
 
 local TableToString do
 	-- Converts a table into a readable string
-	
+
 	local function Parse(Object)
 		local Type = typeof(Object)
 
@@ -25,20 +25,20 @@ local TableToString do
 			Object
 	end
 
-	function TableToString(t)
-		if type(t) == "table" then
+	function TableToString(Table, TableName)
+		if type(Table) == "table" then
 			local IsArrayKey = {}
 			local Output = {}
 			local OutputCount = 0
 
-			for Integer, Value in ipairs(t) do
+			for Integer, Value in ipairs(Table) do
 				IsArrayKey[Integer] = true
 				Output[OutputCount + 1] = Parse(Value)
 				Output[OutputCount + 2] = ", "
 				OutputCount = OutputCount + 2
 			end
 
-			for Key, Value in next, t do
+			for Key, Value in next, Table do
 				if not IsArrayKey[Key] then
 					if type(Key) == "string" and not Key:find("^%d") then
 						Output[OutputCount + 1] = Key
@@ -58,12 +58,16 @@ local TableToString do
 
 			Output[OutputCount] = nil
 
-			local metatable = getmetatable(t)
+			local Metatable = getmetatable(Table)
 
 			Output = "{" .. table.concat(Output) .. "}"
 
-			if metatable then
-				Output = Output .. " <- " .. TableToString(metatable)
+			if Metatable then
+				Output = Output .. " <- " .. TableToString(Metatable)
+			end
+
+			if TableName then
+				Output = TableName .. " = " .. Output
 			end
 
 			return Output
